2352d46228414b39c57ae196c420269a927441ed
Taha Yassine BEN ALI <taha-yassine.benali@passculture.app>
2021-06-03 09:39:46 +0200

    wip

--- orchestrate.py
+++ orchestrate.py
@@ -1,12 +1,14 @@
 from prefect import Flow
 
-from src.data.make_dataset import read_database_and_store_in_parquet
+from src.data.extract_files_from_database import read_database_and_store_in_parquet
 from src.features.feature_engineering import main_feature_engineering
 
 with Flow("Chaîne de traitement") as flow:
-    first_task = read_database_and_store_in_parquet("data/raw/batch_1.db", "data/parquet/train_data")
+    read_first_batch = read_database_and_store_in_parquet("data/raw/batch_1.db", "data/extracted/train_data/batch_1")
+    read_second_batch = read_database_and_store_in_parquet("data/raw/batch_2.db", "data/extracted/train_data/batch_2")
+    read_test_data = read_database_and_store_in_parquet("data/raw/test.db", "data/extracted/test_data")
     second_task = main_feature_engineering()
-    flow.add_edge(first_task, second_task)
+    flow.add_edge(read_first_batch, read_second_batch)
 
 flow.register(project_name="OCTOFLY")
 
