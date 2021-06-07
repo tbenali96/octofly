b5791a529ef0170c6a70a3931f5261779f689bdb
Lea Nacccache <lea.naccache@octo.com>
2021-06-07 16:08:36 +0200

    fix: change a test to green

--- tests/app_visualization/test_metrics_and_KPIs.py
+++ tests/app_visualization/test_metrics_and_KPIs.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 TESTED_MODULE = 'src.app_visualization.metrics_and_KPIs'
 
-def test_get_first_twenty_min_cost_for_airlines__(self):
+def test_get_first_twenty_min_cost_for_airlines__():
     # Given
     expected = None
     # When
