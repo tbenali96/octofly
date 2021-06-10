c165add9a3545bb92a03803e72c3c7054936681a
Taha Yassine BEN ALI <taha-yassine.benali@passculture.app>
2021-06-10 15:30:47 +0200

    mise a jour orchestrate.py + ajout des data

--- orchestrate.py
+++ orchestrate.py
@@ -22,17 +22,19 @@ def aggregate_data(input_filepath: str, output_filepath: str):
     if not os.path.exists(output_filepath):
         os.makedirs(output_filepath)
     batches = os.listdir(input_filepath)
+    if ".DS_Store" in batches:
+        batches.remove(".DS_Store")
     tables = ['aeroports', 'compagnies', 'vols']
     for table in tables:
         logging.info("Aggrégation des données de la table " + str(table))
-        df1 = pd.read_parquet(input_filepath + "/" + str(batches[0]) + "/" + str(table) + ".gzip")
-        df2 = pd.read_parquet(input_filepath + "/" + str(batches[1]) + "/" + str(table) + ".gzip")
+        df1 = pd.read_parquet(f'{input_filepath}/{batches[0]}/{table}.gzip')
+        df2 = pd.read_parquet(f'{input_filepath}/{batches[1]}/{table}.gzip')
         df = concat_dataframes(df1, df2)
         df.to_parquet(output_filepath + "/" + str(table) + '.gzip', compression='gzip')
     # For the dataframe containing the price of the fuel, we keep the parquet raw file
     logging.info("Aggrégation des données de la table prix_fuel")
     df_fuel = pd.read_parquet("data/raw/fuel.parquet")
-    df_fuel.to_parquet(output_filepath + "/prix_fuel.gzip", compression='gzip')
+    df_fuel.to_parquet(f'{output_filepath}/prix_fuel.gzip', compression='gzip')
 
 
 @task
